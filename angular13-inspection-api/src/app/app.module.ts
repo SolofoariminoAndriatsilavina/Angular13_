d781a7644c16d533be72c7a17307ebccf5bf7841
SolofoariminoAndriatsilavina <tsilavinaariminofitahiana@gmail.com>
2022-03-08 13:59:03 +0300

    Generate Component add-edit-inspection in angular

--- angular13-inspection-api/src/app/app.module.ts
+++ angular13-inspection-api/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InspectionComponent } from './inspection/inspection.component';
 import { ShowInspectionComponent } from './inspection/show-inspection/show-inspection.component';
+import { AddEditInspectionComponent } from './inspection/add-edit-inspection/add-edit-inspection.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InspectionComponent,
-    ShowInspectionComponent
+    ShowInspectionComponent,
+    AddEditInspectionComponent
   ],
   imports: [
     BrowserModule,
