1ac5623a20b86a5aa3904aba8b11f1d94c347651
SolofoariminoAndriatsilavina <tsilavinaariminofitahiana@gmail.com>
2022-03-08 13:55:55 +0300

    Generate Component show-inspection in angular

--- angular13-inspection-api/src/app/app.module.ts
+++ angular13-inspection-api/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InspectionComponent } from './inspection/inspection.component';
+import { ShowInspectionComponent } from './inspection/show-inspection/show-inspection.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InspectionComponent
+    InspectionComponent,
+    ShowInspectionComponent
   ],
   imports: [
     BrowserModule,
